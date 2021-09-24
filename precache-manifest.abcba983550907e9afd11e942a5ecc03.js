d50c85a5cdd58260f14338d52435931afc0e4244
Dawid <dawid.bakalarski@gmail.com>
2021-09-24 07:26:11 +0200

    Updates

---
+++ precache-manifest.abcba983550907e9afd11e942a5ecc03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c039386e4b4eb0360cf6fb45a04965f7",
+    "url": "/fav-album-list-2.0/index.html"
+  },
+  {
+    "revision": "e6ec775434e9d0368de9",
+    "url": "/fav-album-list-2.0/static/css/main.b910616e.chunk.css"
+  },
+  {
+    "revision": "8f54e81155a80d8dd2cf",
+    "url": "/fav-album-list-2.0/static/js/2.f9a1b1bf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/fav-album-list-2.0/static/js/2.f9a1b1bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6ec775434e9d0368de9",
+    "url": "/fav-album-list-2.0/static/js/main.cbb73d35.chunk.js"
+  },
+  {
+    "revision": "975d181009628c430a37",
+    "url": "/fav-album-list-2.0/static/js/runtime-main.c6652d70.js"
+  }
+]);
