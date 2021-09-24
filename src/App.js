7e6a6cf1c2c66879321fbe83462f15480251bc42
Dawid <dawid.bakalarski@gmail.com>
2021-09-24 07:35:33 +0200

    fix display empty board

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./App.scss";
 import List from "./Components/List";
 import Form from "./Components/Form";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const albums = useSelector((state) => state.albums.albums);
+  const list = albums.length ? <List /> : null;
+
   return (
     <div className="favMusicList">
       <Form />
-      <List />
+      {list}
     </div>
   );
 };
